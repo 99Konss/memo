aa3c519b63d1f0f44fae88e31c3940cd8310bcb4
99Konss <konstantinwerner99@gmail.com>
2026-07-30 23:09:53 +0200

    Fixed some issues and bugs: Fixed the issue where every line on -l was 1 and a segmentation error that was caused by a missing else statement and an empty if statement

--- main.c
+++ main.c
@@ -25,6 +25,7 @@ void readlist(char *filepath) {
     printf("Record:\n");
     while (fgets(buffer, sizeof(buffer), f) != NULL) {
         printf("  %d: %s", line_number, buffer);
+        line_number++;
     }
     fclose(f);
 }
@@ -36,21 +37,23 @@ void wipelist(char *filepath) {
 
 int main(int argc, char *argv[]) {
     char *home = getenv("HOME");
-
-    if (home == 0) {
-        printf("Error: Could not find HOME directory\n");
-        printf("Exiting...");
+    if (home == NULL) {
+        fprintf(stderr, "Error: Could not find HOME environment variable.\n");
         return 1;
     }
 
     char filepath[256];
     snprintf(filepath, sizeof(filepath), "%s/.reminders/reminders.txt", home);
-    printf("Filepath set to: %s\n", filepath);
+    // printf("Filepath set to: %s\n", filepath); (For debugging purposes)
 
     char reminderinput[100];
 
     if (argc < 2) {
-        // USAGE:
+        printf("Usage:\n");
+        printf("  %s -a     add a new reminder\n", argv[0]);
+        printf("  %s -l     list reminders\n", argv[0]);
+        printf("  %s -c     clear reminder list\n", argv[0]);
+        printf("  %s -h     display help screen\n", argv[0]);
     }
 
     if (strcmp(argv[1], "-a") == 0 || strcmp(argv[1], "--add") == 0) {
@@ -80,6 +83,19 @@ int main(int argc, char *argv[]) {
             printf("Please enter y or n\n");
             return 1;
         }
+    }
+
+    else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+        printf("Usage:\n");
+        printf("  %s -a     add a new reminder\n", argv[0]);
+        printf("  %s -l     list reminders\n", argv[0]);
+        printf("  %s -c     clear reminder list\n", argv[0]);
+        printf("  %s -h     display this screen\n", argv[0]);
+    }
+
+    else {
+        printf("Invalid argument(s): Exiting\n");
+        return 1;
     }
 
     return 0;
